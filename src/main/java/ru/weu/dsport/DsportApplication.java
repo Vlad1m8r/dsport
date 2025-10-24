a1d6c8b9d6fee2d65e6babdfb0bea717ee70b9f0
Vlad1m8r <vlad1m8rgoncharuk@gmail.com>
2025-10-24 17:21:16 +0300

    Init commit

---
+++ src/main/java/ru/weu/dsport/DsportApplication.java
@@ -0,0 +1,13 @@
+package ru.weu.dsport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsportApplication.class, args);
+    }
+
+}
