a5b7c93bda957e6bb0d1a754e8c59408138fd793
Vladimir <39942321+Vlad1m8r@users.noreply.github.com>
2025-10-26 23:51:17 +0300

    Добавить ручку для массива данных

--- src/main/java/ru/weu/dsport/controller/HelloController.java
+++ src/main/java/ru/weu/dsport/controller/HelloController.java
@@ -1,6 +1,7 @@
 package ru.weu.dsport.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -9,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.weu.dsport.dto.GreetingResponse;
+
+import java.util.List;
 
 @RestController
 @Tag(name = "Greeting", description = "Endpoints for greeting visitors of the service.")
@@ -28,5 +31,21 @@ public class HelloController {
             })
     public ResponseEntity<GreetingResponse> getGreeting() {
         return ResponseEntity.ok(new GreetingResponse("Привет, мир!"));
+    }
+
+    @GetMapping("/data")
+    @Operation(
+            summary = "Get sample data array",
+            description = "Returns a sample array of data objects for demonstration purposes.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Array returned successfully.",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = String.class))))
+            })
+    public ResponseEntity<List<String>> getSampleData() {
+        return ResponseEntity.ok(List.of("alpha", "beta", "gamma"));
     }
 }
