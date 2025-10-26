1c9078701e626f897789ce843745dcb7fde3eea4
Vladimir <39942321+Vlad1m8r@users.noreply.github.com>
2025-10-27 00:03:13 +0300

    Add data array endpoint to greeting controller (#5)
    * Добавить ручку для массива данных
    * Вынес описание ручек в интерфейс

--- src/main/java/ru/weu/dsport/controller/HelloController.java
+++ src/main/java/ru/weu/dsport/controller/HelloController.java
@@ -1,32 +1,21 @@
 package ru.weu.dsport.controller;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.weu.dsport.dto.GreetingResponse;
 
-@RestController
-@Tag(name = "Greeting", description = "Endpoints for greeting visitors of the service.")
-public class HelloController {
+import java.util.List;
 
-    @GetMapping("/hello")
-    @Operation(
-            summary = "Get localized greeting",
-            description = "Returns a localized greeting message to confirm the service is available.",
-            responses = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "Greeting returned successfully.",
-                            content = @Content(
-                                    mediaType = "application/json",
-                                    schema = @Schema(implementation = GreetingResponse.class)))
-            })
+@RestController
+public class HelloController implements HelloControllerApi {
+
+    @Override
     public ResponseEntity<GreetingResponse> getGreeting() {
         return ResponseEntity.ok(new GreetingResponse("Привет, мир!"));
+    }
+
+    @Override
+    public ResponseEntity<List<String>> getSampleData() {
+        return ResponseEntity.ok(List.of("alpha", "beta", "gamma"));
     }
 }
