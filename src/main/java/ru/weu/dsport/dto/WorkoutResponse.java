6685e9c7c0b304ec490a6920e0a2c0a6f9bae818
Vladimir <39942321+Vlad1m8r@users.noreply.github.com>
2025-11-08 22:45:33 +0300

    Устранил замечания к DTO тренировок

--- src/main/java/ru/weu/dsport/dto/WorkoutResponse.java
+++ src/main/java/ru/weu/dsport/dto/WorkoutResponse.java
@@ -6,13 +6,32 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Schema(description = "Информация о тренировке с перечнем упражнений")
-public record WorkoutResponse(
-        @Schema(description = "Название тренировки", example = "Кардио утро")
-        String title,
-        @Schema(description = "Дата проведения тренировки", example = "2024-04-01")
-        LocalDate date,
-        @Schema(description = "Список упражнений, входящих в тренировку", type = "array",
-                example = "[\\"Берпи\\", \\"Планка\\", \\"Бег на месте\\"]")
-        List<String> exercises
-) {
+public class WorkoutResponse {
+
+    @Schema(description = "Название тренировки", example = "Кардио утро")
+    private final String title;
+
+    @Schema(description = "Дата проведения тренировки", example = "2024-04-01")
+    private final LocalDate date;
+
+    @Schema(description = "Список упражнений, входящих в тренировку", example = "[Берпи, Планка, Бег на месте]")
+    private final List<String> exercises;
+
+    public WorkoutResponse(String title, LocalDate date, List<String> exercises) {
+        this.title = title;
+        this.date = date;
+        this.exercises = exercises;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public List<String> getExercises() {
+        return exercises;
+    }
 }
