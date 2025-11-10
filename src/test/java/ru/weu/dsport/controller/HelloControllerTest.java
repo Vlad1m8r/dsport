7dd751ae821d0f4ac28a9b59a8a209bcabf5d096
Vladimir <39942321+Vlad1m8r@users.noreply.github.com>
2025-11-11 00:36:35 +0300

    Подключение базового интеграционного теста

--- src/test/java/ru/weu/dsport/controller/HelloControllerTest.java
+++ src/test/java/ru/weu/dsport/controller/HelloControllerTest.java
@@ -8,13 +8,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.weu.dsport.test.AbstractIntegrationTest;
 
-@SpringBootTest
 @AutoConfigureMockMvc
-class HelloControllerTest {
+class HelloControllerTest extends AbstractIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
